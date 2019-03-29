ee281400a8bd367c7da608c8b050e051cf7237a8
José Luis de la Hoz <joseluisdelahozgarcia@gmail.com>
2019-03-29 14:23:15 +0100

    Repair project and deleted some worthless vhd files

--- practica1/practica1.sim/sim_1/behav/xsim/xsim.dir/tb_codificador_behav/webtalk/xsim_webtalk.tcl
+++ practica1/practica1.sim/sim_1/behav/xsim/xsim.dir/tb_codificador_behav/webtalk/xsim_webtalk.tcl
@@ -1,6 +1,6 @@
-webtalk_init -webtalk_dir C:/Users/joseluis/Desktop/Redes/practica1/practica1.sim/sim_1/behav/xsim/xsim.dir/tb_decodificador_behav/webtalk/
+webtalk_init -webtalk_dir C:/Users/joseluis/Desktop/Redes/practica1/practica1.sim/sim_1/behav/xsim/xsim.dir/tb_codificador_behav/webtalk/
 webtalk_register_client -client project
-webtalk_add_data -client project -key date_generated -value "Fri Mar 29 14:19:46 2019" -context "software_version_and_target_device"
+webtalk_add_data -client project -key date_generated -value "Fri Mar 29 14:19:45 2019" -context "software_version_and_target_device"
 webtalk_add_data -client project -key product_version -value "XSIM v2018.3 (64-bit)" -context "software_version_and_target_device"
 webtalk_add_data -client project -key build_version -value "2405991" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_platform -value "WIN64" -context "software_version_and_target_device"
@@ -14,7 +14,7 @@ webtalk_add_data -client project -key target_package -value "not_applicable" -co
 webtalk_add_data -client project -key target_speed -value "not_applicable" -context "software_version_and_target_device"
 webtalk_add_data -client project -key random_id -value "f5acb16e-db09-4799-9ced-3d76e294b294" -context "software_version_and_target_device"
 webtalk_add_data -client project -key project_id -value "e81b173a353e4952b20fb382f630bc6a" -context "software_version_and_target_device"
-webtalk_add_data -client project -key project_iteration -value "15" -context "software_version_and_target_device"
+webtalk_add_data -client project -key project_iteration -value "4" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_name -value "Microsoft Windows 8 or later , 64-bit" -context "user_environment"
 webtalk_add_data -client project -key os_release -value "major release  (build 9200)" -context "user_environment"
 webtalk_add_data -client project -key cpu_name -value "Intel(R) Core(TM) i5-7360U CPU @ 2.30GHz" -context "user_environment"
@@ -26,7 +26,7 @@ webtalk_add_data -client xsim -key Command -value "xsim" -context "xsim\\command
 webtalk_add_data -client xsim -key trace_waveform -value "true" -context "xsim\\usage"
 webtalk_add_data -client xsim -key runtime -value "1 us" -context "xsim\\usage"
 webtalk_add_data -client xsim -key iteration -value "1" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Time -value "0.01_sec" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Memory -value "5508_KB" -context "xsim\\usage"
-webtalk_transmit -clientid 3799459102 -regid "" -xml C:/Users/joseluis/Desktop/Redes/practica1/practica1.sim/sim_1/behav/xsim/xsim.dir/tb_decodificador_behav/webtalk/usage_statistics_ext_xsim.xml -html C:/Users/joseluis/Desktop/Redes/practica1/practica1.sim/sim_1/behav/xsim/xsim.dir/tb_decodificador_behav/webtalk/usage_statistics_ext_xsim.html -wdm C:/Users/joseluis/Desktop/Redes/practica1/practica1.sim/sim_1/behav/xsim/xsim.dir/tb_decodificador_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
+webtalk_add_data -client xsim -key Simulation_Time -value "0.05_sec" -context "xsim\\usage"
+webtalk_add_data -client xsim -key Simulation_Memory -value "5512_KB" -context "xsim\\usage"
+webtalk_transmit -clientid 804447111 -regid "" -xml C:/Users/joseluis/Desktop/Redes/practica1/practica1.sim/sim_1/behav/xsim/xsim.dir/tb_codificador_behav/webtalk/usage_statistics_ext_xsim.xml -html C:/Users/joseluis/Desktop/Redes/practica1/practica1.sim/sim_1/behav/xsim/xsim.dir/tb_codificador_behav/webtalk/usage_statistics_ext_xsim.html -wdm C:/Users/joseluis/Desktop/Redes/practica1/practica1.sim/sim_1/behav/xsim/xsim.dir/tb_codificador_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
 webtalk_terminate
