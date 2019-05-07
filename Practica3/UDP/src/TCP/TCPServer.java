7c428f7c524945e643164c24a7c7a52d46a93fe3
José Luis de la Hoz <joseluisdelahozgarcia@gmail.com>
2019-05-07 18:53:50 +0200

    TCP commented

--- Practica3/UDP/src/TCP/TCPServer.java
+++ Practica3/UDP/src/TCP/TCPServer.java
@@ -3,18 +3,26 @@ package TCP;
 import java.io.IOException;
 import java.net.*;
 
+/**
+ * TCPServer manage of running a TCP server.   
+ */
 public class TCPServer {
 
+	/**
+	 * Start the TCPServer in a unlimited loop.
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		ServerSocket listenSocket = null;
 		try {
-			int serverPort = 7896;
-			listenSocket = new ServerSocket(serverPort);
+			int serverPort = 7896;	// Assigns the number of the port (server)
+			listenSocket = new ServerSocket(serverPort); // initialise the socket
 			while(true) {
-				Socket clientSocket = listenSocket.accept();
-				if(clientSocket.isConnected()) {
-					System.out.println("New login");
-					Connection connection = new Connection(clientSocket);
+				Socket clientSocket = listenSocket.accept(); // accepts a connection from the client
+				if(clientSocket.isConnected()) {	// listen socket checks if it is connected 
+					System.out.println("New login"); // Console Message to verify
+					Connection connection = new Connection(clientSocket); // Initialize the connection between client and server
+					connection.start();	// Start the logic of the server
 				}
 			}
 		} catch (IOException e) {
