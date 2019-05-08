ad95ed64e6730f68abf5de49ac99c460f0fa9b19
José Luis de la Hoz <joseluisdelahozgarcia@gmail.com>
2019-05-08 15:25:23 +0200

    Distance vector algorithm

--- Practica2/src/algorithm/DistanceVector.java
+++ Practica2/src/algorithm/DistanceVector.java
@@ -1,4 +1,44 @@
 package algorithm;
 
+import data.structure.Graph;
+import data.structure.Router;
+
+/**
+ *  DistanceVector manages to create all the routers of the network and have methods
+ *  to the execution of a Bellman Ford algorithm.
+ */
 public class DistanceVector {
+	
+	private Graph network;
+	private Router[] routers;
+	
+	public DistanceVector(Graph graph) {
+		network = graph;
+		routers = new Router[graph.getNumberOfNodes()];
+		initialiseRouters();
+	}
+	
+	/**
+	 * Set all routers neighbours in their respective tables.
+	 */
+	public void setNeighbourRoutersTable() {
+		for(int i = 0; i < routers.length; i++) {
+			routers[i].setNeighbours();
+		}
+	}
+	
+	@Override
+	public String toString() {
+		StringBuilder output = new StringBuilder();
+		for(int i = 0; i < routers.length; i++) {
+			output.append(routers[i] + "\n\n");
+		}
+		return output.toString();
+	}
+	
+	private void initialiseRouters() {
+		for(int i = 0; i < routers.length; i++) {
+			routers[i] = new Router(network, i);
+		}
+	}
 }
